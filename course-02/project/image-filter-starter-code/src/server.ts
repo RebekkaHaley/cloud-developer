324aaeabb91028e03cdbe18b4ffa2cb5b3da2a27
Rebekka Haley <rebekka.haley@gmail.com>
2020-05-05 18:27:26 +0100

    Unstubbed the TODO code for Udacity project c2

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -16,8 +16,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
+  // IT SHOULD:
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -26,6 +25,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  app.get("/filteredimage", async (req, res) => {
+    let {image_url} = req.query;
+
+    if (!image_url) {
+      return res.status(400).send('url is required');
+    }
+
+    let filteredImage : string;
+    await filterImageFromURL(image_url).then( path => {
+      filteredImage = path;
+    });
+
+    return res.status(200).sendFile(filteredImage, function () {
+      deleteLocalFiles([filteredImage]);
+    });
+    
+  });
 
   /**************************************************************************** */
 
