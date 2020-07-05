60580c16c1bc71ec5521fbbef765a7c94e33a17c
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-05 19:37:31 +0100

    [uda] lesson 2 / updated pagination code

--- course-04/exercises/lesson-2/starter-code/index.js
+++ course-04/exercises/lesson-2/starter-code/index.js
@@ -10,20 +10,33 @@ exports.handler = async (event) => {
   console.log('Processing event: ', event)
 
   // TODO: Read and parse "limit" and "nextKey" parameters from query parameters
-  // let nextKey // Next key to continue scan operation if necessary
-  // let limit // Maximum number of elements to return
+  let nextKey  // Next key to continue scan operation if necessary
+  let limit  // Maximum number of elements to return
 
-  // HINT: You might find the following method useful to get an incoming parameter value
-  // getQueryParameter(event, 'param')
-
-  // TODO: Return 400 error if parameters are invalid
+  try{
+    // Parse query parameters
+    nextKey = parseNextKeyParameter(event)
+    limit = parseLimitParameter(event) || 20  // 20 is default
+  } catch (e) {
+    // TODO: Return 400 error if parameters are invalid
+    console.log('Failed to parse query parameters: ', e.message)
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'Invalid parameters'
+      })
+    }
+  }
 
   // Scan operation parameters
   const scanParams = {
     TableName: groupsTable,
     // TODO: Set correct pagination parameters
-    // Limit: ???,
-    // ExclusiveStartKey: ???
+    Limit: limit,
+    ExclusiveStartKey: nextKey
   }
   console.log('Scan params: ', scanParams)
 
@@ -45,6 +58,35 @@ exports.handler = async (event) => {
       nextKey: encodeNextKey(result.LastEvaluatedKey)
     })
   }
+}
+
+// HINT: You might find the following method useful to get an incoming parameter value
+// getQueryParameter(event, 'param')
+
+// Define 'parseNextKeyParameter' function
+function parseNextKeyParameter(event) {
+  const nextKeyStr = getQueryParameter(event, 'nextKey')
+  if (!nextKeyStr) {
+    return undefined
+  }
+
+  const uriDecoded = decodeURIComponent(nextKeyStr)
+  return JSON.parse(uriDecoded)
+}
+
+// Define 'parseLimitParameter' function
+function parseLimitParameter(event) {
+  const limitStr = getQueryParameter(event, 'limit')
+  if (!limitStr) {
+    return undefined
+  }
+
+  const limit = parseInt(limitStr, 10)
+  if (!limit <= 0) {
+    throw new Error('Limit should be positive')
+  }
+
+  return limit
 }
 
 /**
