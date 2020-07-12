fee7ef377570e10911583d83a6a57db95abea694
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-12 17:53:25 +0100

    [udalesson5] updated config for react app (client)

--- course-04/exercises/c4-client-master/src/config.ts
+++ course-04/exercises/c4-client-master/src/config.ts
@@ -1,8 +1,8 @@
 const apiId = '5sjtoelkz4' // My serverless app
-export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev`
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev` // NB: must be eu
 
 export const authConfig = {
-  domain: 'test-endpoint.auth0.com',
+  domain: 'dev-eb-o8fgn.eu.auth0.com', // My autho0 app domain
   clientId: 'SENzCCFNi515FY7zbr7AajB57rLePUVV', // My autho0 app
   callbackUrl: 'http://localhost:3000/callback'
 }
