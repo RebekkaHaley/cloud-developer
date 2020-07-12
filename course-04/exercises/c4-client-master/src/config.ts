5f7434b0aad9a2ceaed1d9f0989532a15051fbc8
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-12 17:07:00 +0100

    [udalesson5] updated app id in web client config

--- course-04/exercises/c4-client-master/src/config.ts
+++ course-04/exercises/c4-client-master/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '5sjtoelkz4'
+const apiId = '5sjtoelkz4' // My serverless app
 export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'test-endpoint.auth0.com',
-  clientId: 'A72UMiQQJzmHQRPpe1dHOyq3EfPvo3Hy',
+  clientId: 'SENzCCFNi515FY7zbr7AajB57rLePUVV', // My autho0 app
   callbackUrl: 'http://localhost:3000/callback'
 }
