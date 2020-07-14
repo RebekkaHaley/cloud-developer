6eab03f9e54087bd30a6eea19c4fbedd65980aa7
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-14 23:57:39 +0100

    [udalesson6] updated auth0 to async version

--- course-04/exercises/c4-client-master/src/config.ts
+++ course-04/exercises/c4-client-master/src/config.ts
@@ -3,6 +3,6 @@ export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com
 
 export const authConfig = {
   domain: 'dev-eb-o8fgn.eu.auth0.com', // My autho0 app domain
-  clientId: 'SENzCCFNi515FY7zbr7AajB57rLePUVV', // My autho0 app
+  clientId: 'b7O7XqcbYQSMdjOurmJ60OWPo74oZjT2', // My autho0 app
   callbackUrl: 'http://localhost:3000/callback'
 }
